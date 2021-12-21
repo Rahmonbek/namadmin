72240fa4c1b89926566ce8e4abd057d557798f28
Developer <62201299+Developer06092000@users.noreply.github.com>
2021-12-22 01:14:59 +0500

    Update documents.js

--- my-app/src/pages/documents.js
+++ my-app/src/pages/documents.js
@@ -1,38 +1,235 @@
 import React, { Component } from "react";
 import Loader from "./Loader";
-import { Modal, Button } from "antd";
+import { Modal, Select, Button, Input, Space, Table } from "antd";
 import { Form } from "react-bootstrap";
-import ImageDemo from "./ImageDemo";
+import Highlighter from "react-highlight-words";
+import {
+  SearchOutlined,
+  EyeOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
 
 export default class Documents extends Component {
   state = {
     loading: true,
     show: false,
+    title: "",
+    type: "Shartnoma",
     file: null,
+    searchText: "",
+    searchedColumn: "",
+    files: [
+      {
+        id: 1,
+        title: "Shartnoma",
+        file: "https://staff.tiiame.uz/storage/users/14/presentations/xeiF6kKGT1NUUNhjhST2w6AP2xlkpUNr1WkEvU4Z.pdf",
+        type: "Shartnoma",
+      },
+      {
+        id: 2,
+        title: "Qonun",
+        file: "https://staff.tiiame.uz/storage/users/14/presentations/xeiF6kKGT1NUUNhjhST2w6AP2xlkpUNr1WkEvU4Z.pdf",
+        type: "Qonunchilik",
+      },
+      {
+        id: 3,
+        title: "Sertifikat",
+        file: "https://staff.tiiame.uz/storage/users/14/presentations/xeiF6kKGT1NUUNhjhST2w6AP2xlkpUNr1WkEvU4Z.pdf",
+        type: "Sertifikat",
+      },
+    ],
   };
+
+  filterFiles = (documents) => {
+    var files = documents;
+    for (let i = 0; i < files.length; i++) {
+      files[i].key = i + 1;
+    }
+    this.setState({ files });
+  };
+
+  getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={(node) => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            this.handleSearch(selectedKeys, confirm, dataIndex)
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => this.handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              this.setState({
+                searchText: selectedKeys[0],
+                searchedColumn: dataIndex,
+              });
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : "",
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select(), 100);
+      }
+    },
+    render: (text) =>
+      this.state.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[this.state.searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    this.setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  handleReset = (clearFilters) => {
+    clearFilters();
+    this.setState({ searchText: "" });
+  };
+
   openModal = () => {
     this.setState({
       show: true,
     });
   };
+
   closeModal = () => {
     this.setState({
       show: false,
-      image: null,
-      imageUrl: null,
-      edit: null,
+      type: "Shartnoma",
+      file: null,
+      title: "",
     });
-    document.getElementById("formBasicfile").value = "";
-    document.getElementById("formBasictext").value = "";
-    document.getElementById("formBasictitle").value = "";
   };
+
   customRequest = (e) => {
-    console.log(e.target.files[0]);
     this.setState({
       file: e.target.files[0],
     });
   };
+
+  createDocument = () => {
+    console.log(this.state.type, this.state.title, this.state.file);
+  };
+
+  componentDidMount() {
+    this.filterFiles(this.state.files);
+  }
+
   render() {
+    const columns = [
+      {
+        title: "T/r",
+        dataIndex: "key",
+        key: "key",
+      },
+      {
+        title: "Hujjat nomi",
+        dataIndex: "title",
+        key: "title",
+        ...this.getColumnSearchProps("title"),
+      },
+      {
+        title: "Hujjat turi",
+        dataIndex: "type",
+        key: "type",
+        ...this.getColumnSearchProps("type"),
+      },
+      {
+        title: "Hujjat fayli",
+        dataIndex: "file",
+        key: "file",
+        render: (file) => {
+          return (
+            <Button type="primary" href={file} icon={<EyeOutlined />}>
+              Ko'rish
+            </Button>
+          );
+        },
+      },
+      {
+        title: "O'zgartirish",
+        dataIndex: "key",
+        key: "key",
+        render: (key) => {
+          return (
+            <Button type="primary" icon={<EditOutlined />}>
+              O'zgartirish
+            </Button>
+          );
+        },
+      },
+      {
+        title: "O'chirish",
+        dataIndex: "key",
+        key: "key",
+        render: (key) => {
+          return (
+            <Button type="danger" icon={<DeleteOutlined />}>
+              O'chirish
+            </Button>
+          );
+        },
+      },
+    ];
     return (
       <div>
         {this.state.loading !== true ? (
@@ -42,6 +239,8 @@ export default class Documents extends Component {
             <Button type="primary" onClick={this.openModal}>
               Hujjat qo'shish
             </Button>
+            <br />
+            <br />
             <Modal
               title="Hujjat"
               visible={this.state.show}
@@ -54,7 +253,8 @@ export default class Documents extends Component {
                   <br />
                   <Form.Control
                     className="formInput"
-                    defaultValue={this.state.title}
+                    value={this.state.title}
+                    onChange={(e) => this.setState({ title: e.target.value })}
                     name="title"
                     required
                     type="text"
@@ -76,18 +276,21 @@ export default class Documents extends Component {
                 </Form.Group>
 
                 <Form.Group
-                  controlId="formBasictext"
+                  controlId="formBasictype"
                   className="mb-3"
                   style={{ width: "100%" }}
                 >
-                  <Form.Label>Hujjat tipi</Form.Label>
+                  <Form.Label>Hujjat turi</Form.Label>
                   <br />
-                  <Form.Control
-                    className="formInput"
-                    defaultValue={this.state.textF}
-                    name="text"
-                    placeholder="Hujjat tipi"
-                  />
+                  <Select
+                    value={this.state.type}
+                    onSelect={(value) => this.setState({ type: value })}
+                    style={{ width: "100%" }}
+                  >
+                    <option value="Shartnoma">Shartnoma</option>
+                    <option value="Qonunchilik">Qonunchilik</option>
+                    <option value="Sertifikat">Sertifikat</option>
+                  </Select>
                 </Form.Group>
                 <br />
                 <br />
@@ -101,12 +304,13 @@ export default class Documents extends Component {
                 <Button
                   type="primary"
                   htmlType="button"
-                  onClick={this.createNew}
+                  onClick={this.createDocument}
                 >
                   Yaratish
                 </Button>
               </Form>
             </Modal>
+            <Table columns={columns} dataSource={this.state.files} />
           </div>
         )}
       </div>
