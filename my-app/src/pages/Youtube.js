07b6169283706dab2a1e49a3c55565b6cbbaded8
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-12-24 18:24:10 +0500

    wfegerge

--- my-app/src/pages/Youtube.js
+++ my-app/src/pages/Youtube.js
@@ -1,4 +1,4 @@
-import { Button, message } from 'antd';
+import { message } from 'antd';
 import axios from 'axios';
 import React, { Component } from 'react'
 import { Col, Row } from 'react-bootstrap';
@@ -6,18 +6,29 @@ import { Redirect } from 'react-router-dom';
 import YouTube from "@u-wave/react-youtube";
 import { url } from '../host/Host';
 import Loader from './Loader';
+import { Button, Space } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 export default class Youtube extends Component {
     state={
         school:null,
-        loading:true
+        loading:true,
+        edit:null,
     }
     addVideo =()=>{
-        var a=document.getElementById('you').value
-        var videos=[a.slice(a.lastIndexOf('/')+1),a.slice(a.lastIndexOf('/')+1)]
+      var a=document.getElementById('you').value
+        var videos=this.state.school.youtube_videos
         
-        var config={youtube_videos:videos}
+      if(this.state.edit===null){
+        videos.push(a.slice(a.lastIndexOf('/')+1))
+        
        
+      }else{
+videos[this.state.edit]=a.slice(a.lastIndexOf('/')+1)
+      }
+      
+      var config={youtube_videos:videos}
+        
         axios
         .patch(`${url}/boshqarma/${1}/`, config, {
   
@@ -25,6 +36,7 @@ export default class Youtube extends Component {
            'Authorization': `Token ${window.localStorage.getItem("token")}`
           }})
         .then((res) => {
+         
           this.getSchool();
           message.success("Ma'lumot qo'shildi");
         })
@@ -32,6 +44,10 @@ export default class Youtube extends Component {
           message.error("Ma'lumot qo'shilmadi");
           this.setState({ loading: false });
         });
+        this.setState({
+          edit:null
+        })
+        
     }
     componentDidMount() {
         this.getSchool();
@@ -49,8 +65,31 @@ export default class Youtube extends Component {
         });
       };
     
-     
-    
+     editVideo=(id)=>{
+       this.setState({
+         edit:id
+       })
+     }
+    deleteVideo=(id)=>{
+      var videos=this.state.school.youtube_videos
+      videos.splice(id, 1)
+      axios
+      .patch(`${url}/boshqarma/${1}/`, videos, {
+
+        headers: {
+         'Authorization': `Token ${window.localStorage.getItem("token")}`
+        }})
+      .then((res) => {
+       
+        this.getSchool();
+        message.success("Ma'lumot o'chirildi");
+      })
+      .catch((err) => {
+        message.success("Ma'lumot o'chirildi");
+        this.setState({ loading: false });
+      });
+      
+    }
     render() {
         return (
             <div>
@@ -61,6 +100,7 @@ export default class Youtube extends Component {
         ) : (<div>
             <div>
                 <form  className='formnew'>
+                  <h3>{this.state.edit===null?<p>Yangi video qo'shish</p>:<p>Tanlangan videoni almashtirish</p>}</h3>
                     <input type="url" placeholder='Videoni linkini kiriting' id="you" style={{width:'100%', display:'block', fontSize:'20px'}}/>
                 <br/>
                 <Button type="primary" onClick={this.addVideo} htmlType='button'>Videoni qo'shish</Button>
@@ -70,13 +110,32 @@ export default class Youtube extends Component {
             this.state.school!==null?
             this.state.school.youtube_videos.map((item,key)=>{
                 return(
-                <Col lg={3} md={4} sm={6} style={{padding:'20px'}}>
+                <Col lg={6} md={12} sm={12} style={{padding:'20px'}}>
+               <div className="you_col" >
                 <YouTube
-            
+            style={{width:'100%', height:'300px'}}
   video={item}                 
   className="you"                
-  
+  autoPlay={true}
+  muted={true}
   />
+  <div className="you_btn">  <Button
+            type="primary"
+            icon={<EditOutlined style={{position:'relative', top:'-5px'}} />}
+          onClick={()=>{this.editVideo(key)}}  
+            
+          >
+            O'zgartirish
+          </Button>
+          <Button
+            type="danger"
+            icon={<DeleteOutlined style={{position:'relative', top:'-5px'}} />}
+            onClick={()=>{this.deleteVideo(key)}} 
+           
+          >
+            O'chirish
+          </Button></div>
+          </div>
                 </Col>)
             })
             :''
